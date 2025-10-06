a004a4eda9b21101b298a24b352e016e1360017a
Abimbola <bimbolaoladunjoye74@gmail.com>
2025-10-06 01:55:29 +0100

    first commit

---
+++ lib/const/resource.dart
@@ -0,0 +1,10 @@
+/// Generate by [asset_generator](https://github.com/fluttercandies/flutter_asset_generator) library.
+/// PLEASE DO NOT EDIT MANUALLY.
+// ignore_for_file: constant_identifier_names
+class R {
+  const R._();
+
+  /// ![preview](file://C:\Users\HP\Desktop\New folder (2)\fact200\assets\images\Geography.png)
+  static const String ASSETS_IMAGES_GEOGRAPHY_PNG =
+      'assets/images/Geography.png';
+}
